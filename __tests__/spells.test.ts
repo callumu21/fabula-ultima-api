3eb70d591c67f3a6e5740ef9c2087cd830eeef4d
Callum Underwood <cmu.wgs@gmail.com>
2025-06-20 15:07:50 +0100

    Add route for fetching individual spell by ID

--- __tests__/spells.test.ts
+++ __tests__/spells.test.ts
@@ -105,3 +105,81 @@ describe('GET /spells', () => {
     expect(body.spells).toHaveLength(0);
   });
 });
+
+describe('GET /spells/:id', () => {
+  const server = buildServer();
+
+  beforeAll(async () => {
+    await server.ready();
+  });
+
+  beforeEach(async () => {
+    const tableNames = await prisma.$queryRaw<
+      Array<{ tablename: string }>
+    >`SELECT tablename FROM pg_tables WHERE schemaname='public'`;
+
+    const truncationPromises = tableNames.map(({ tablename }) => {
+      if (tablename !== '_prisma_migrations') {
+        return prisma
+          .$executeRawUnsafe(`TRUNCATE TABLE "public"."${tablename}" CASCADE;`)
+          .catch((err) => {
+            console.error(err);
+          });
+      }
+      return null;
+    });
+
+    await Promise.all(truncationPromises);
+  });
+
+  afterAll(async () => {
+    await server.close();
+    return prisma.$disconnect();
+  });
+
+  it('returns a single matching spell and class information from the database', async () => {
+    const mockClass = createMockClass();
+
+    await prisma.class.create({
+      data: mockClass,
+    });
+
+    const elementalShroudSpell = createMockSpell({ id: 'elemental-shroud' });
+
+    await prisma.spell.create({ data: elementalShroudSpell });
+
+    const res = await server.inject({
+      method: 'GET',
+      url: '/spells/elemental-shroud',
+    });
+
+    expect(res.statusCode).toBe(200);
+    const body = JSON.parse(res.body);
+
+    const { spell } = body;
+
+    expect(spell).toEqual({ ...elementalShroudSpell, class: mockClass });
+  });
+
+  it('returns a 404 and message if no matching spell exists', async () => {
+    const mockClass = createMockClass();
+
+    await prisma.class.create({
+      data: mockClass,
+    });
+
+    const elementalShroudSpell = createMockSpell({ id: 'elemental-shroud' });
+
+    await prisma.spell.create({ data: elementalShroudSpell });
+
+    const res = await server.inject({
+      method: 'GET',
+      url: '/spells/not-a-spell',
+    });
+
+    expect(res.statusCode).toBe(404);
+    const body = JSON.parse(res.body);
+
+    expect(body.msg).toEqual('Spell not found');
+  });
+});
--- src/routes/spells.ts
+++ src/routes/spells.ts
@@ -7,4 +7,19 @@ export async function spellRoutes(server: FastifyInstance) {
     });
     return { spells };
   });
+
+  server.get('/spells/:id', async (req, res) => {
+    const { id } = req.params as { id: string };
+
+    const spell = await server.prisma.spell.findUnique({
+      where: { id },
+      include: { class: true },
+    });
+
+    if (!spell) {
+      return res.status(404).send({ msg: 'Spell not found' });
+    }
+
+    return { spell };
+  });
 }
