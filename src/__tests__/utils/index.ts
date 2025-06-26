efcd771a30f433b44ee782fa3af1ec90504d7541
Callum Underwood <cmu.wgs@gmail.com>
2025-06-26 17:07:09 +0100

    Extract logic for resetting the db between tests in a util func

--- src/__tests__/utils/index.ts
+++ src/__tests__/utils/index.ts
@@ -1,4 +1,24 @@
+import { PrismaClient } from '../../../generated/prisma';
 import { FastifyInstance } from 'fastify';
+
+export const resetDb = async (prismaInstance: PrismaClient): Promise<void> => {
+  const tableNames = await prismaInstance.$queryRaw<
+    Array<{ tablename: string }>
+  >`SELECT tablename FROM pg_tables WHERE schemaname='public'`;
+
+  const truncationPromises = tableNames.map(({ tablename }) => {
+    if (tablename !== '_prisma_migrations') {
+      return prismaInstance
+        .$executeRawUnsafe(`TRUNCATE TABLE "public"."${tablename}" CASCADE;`)
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+    return null;
+  });
+
+  await Promise.all(truncationPromises);
+};
 
 type PlayerDetails = {
   userId: number;
