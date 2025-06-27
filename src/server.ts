0a7e15b54780eeb3115fe338c77ec5ac143b881e
Callum Underwood <cmu.wgs@gmail.com>
2025-06-27 17:22:28 +0100

    Set up custom validation error message logging for better consistency

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import Fastify from 'fastify';
+import ajvErrors from 'ajv-errors';
 import authPlugin from './plugins/auth';
 import prismaPlugin from './plugins/prisma';
 import authRoutes from './routes/auth';
@@ -8,7 +9,16 @@ import spellRoutes from './routes/spells';
 import weaponRoutes from './routes/weapons';
 
 const buildServer = () => {
-  const app = Fastify();
+  const app = Fastify({
+    ajv: {
+      customOptions: { allErrors: true, coerceTypes: false },
+      plugins: [ajvErrors],
+    },
+    schemaErrorFormatter: (errors) => {
+      const [firstError] = errors;
+      return new Error(firstError.message || 'Invalid input.');
+    },
+  });
 
   app.register(authPlugin);
   app.register(prismaPlugin);
@@ -23,6 +33,17 @@ const buildServer = () => {
   app.register(skillRoutes);
   app.register(spellRoutes);
   app.register(weaponRoutes);
+
+  app.setErrorHandler((err, req, reply) => {
+    if (err.validation) {
+      return reply.status(400).send({
+        statusCode: 400,
+        msg: err.message,
+      });
+    }
+
+    return reply.send(err);
+  });
 
   return app;
 };
