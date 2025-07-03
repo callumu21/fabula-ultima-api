789cb7b191ad1858b22bd2f0f0c95e66cc616a5c
Callum Underwood <cmu.wgs@gmail.com>
2025-07-03 13:52:42 +0100

    Remove id from status effect typing

--- src/typings/status-effects.ts
+++ src/typings/status-effects.ts
@@ -3,7 +3,6 @@ export type Attribute = 'DEX' | 'INS' | 'MIG' | 'WLP';
 export type StatusEffectName = 'Slow' | 'Dazed' | 'Weak' | 'Shaken' | 'Enraged' | 'Poisoned';
 
 export interface StatusEffect {
-  id: string;
   name: StatusEffectName;
   affectedAttributes: Attribute[];
 }
